105d0e80d8cb2d8b84180db380f5fcd84566bb55
Risina Perera <risina@calcey.com>
2017-02-04 13:35:32 +0530

    Making the latest version Objective-C compatible.

--- Example/FusumaExample/ViewController.swift
+++ Example/FusumaExample/ViewController.swift
@@ -95,7 +95,7 @@ class ViewController: UIViewController, FusumaDelegate {
         print("Called when the FusumaViewController disappeared")
     }
     
-    func fusumaWillClose() {
+    func fusumaWillClosed() {
         print("Called when the close button is pressed")
     }
 
