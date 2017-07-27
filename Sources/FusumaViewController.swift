c7d629820b3565f491d4ffa9994ec5ecde32de2b
ytakzk <yt@ytakzk.me>
2017-07-27 21:11:39 +0900

    fix #186

--- Sources/FusumaViewController.swift
+++ Sources/FusumaViewController.swift
@@ -564,7 +564,6 @@ private extension FusumaViewController {
             break
         }
         
-        doneButton.isHidden = !hasGalleryPermission
         self.view.bringSubview(toFront: menuView)
     }
     
