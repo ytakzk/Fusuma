3ebcda3d801be50f95c1a1dc9cd5c0af03fc9013
PJ Vea <pj.vea@busybusy.com>
2018-06-26 10:35:35 -0600

    Added checkmark to photo selection.

--- Fusuma.podspec
+++ Fusuma.podspec
@@ -1,6 +1,6 @@
 Pod::Spec.new do |s|
   s.name             = "Fusuma"
-  s.version          = "1.2.9"
+  s.version          = "1.2.10"
   s.summary          = "Instagram-like photo browser with a few line of code written in Swift"
   s.homepage         = "https://github.com/ytakzk/Fusuma"
   s.license          = 'MIT'
--- Sources/FSConstants.swift
+++ Sources/FSConstants.swift
@@ -33,7 +33,7 @@ extension UIView {
     func addBottomBorder(_ color: UIColor, width: CGFloat) {
         let border = CALayer()
         border.borderColor = color.cgColor
-        border.frame = CGRect(x: 0, y: self.frame.size.height - width, width:  self.frame.size.width, height: width)
+        border.frame = CGRect(x: 0, y: self.frame.size.height - width, width: UIScreen.main.bounds.width, height: width)
         border.borderWidth = width
         self.layer.addSublayer(border)
     }
